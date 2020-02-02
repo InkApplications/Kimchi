db96d31e50e1d1da22de22a01ece25df1740c719
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-02-02 11:29:07 -0600

    Add Kotlin 1.3.61

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+    }
+}
+
