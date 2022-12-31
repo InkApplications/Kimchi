623fbdcdb53ff448f447950ca309156684493a4c
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-12-31 10:41:19 -0600

    Explicitly specify logger publication to fix android publications

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("ink.multiplatform")
-    id( "com.android.library")
-    id("com.inkapplications.publishing")
+    id("com.android.library")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -16,7 +17,6 @@ android {
 kotlin {
     android {
         publishAllLibraryVariants()
-        publishLibraryVariantsGroupedByFlavor = true
     }
 
     sourceSets {
@@ -37,6 +37,56 @@ kotlin {
             dependencies {
                 implementation(libs.kotlin.test.core)
                 implementation(libs.kotlin.test.junit)
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            val versionArg = when (project.properties["version"]?.toString()) {
+                null, "unspecified", "" -> "1.0-SNAPSHOT"
+                else -> project.properties["version"].toString()
+            }
+
+            create<MavenPublication>("release") {
+                from(components["release"])
+                version = versionArg
+            }
+        }
+        repositories {
+            maven {
+                name = "Build"
+                url = uri(layout.buildDirectory.dir("repo"))
+            }
+
+            val mavenUser: String? by project
+            val mavenPassword: String? by project
+            if (mavenUser != null && mavenPassword != null) {
+                maven {
+                    name = "MavenCentral"
+                    url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = mavenUser
+                        password = mavenPassword
+                    }
+                }
+            }
+        }
+        signing {
+            val signingKeyId: String? by project
+            val signingKey: String? by project
+            val signingPassword: String? by project
+
+            if (signingKey != null) {
+                if (signingKeyId != null) {
+                    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+                } else {
+                    useInMemoryPgpKeys(signingKey, signingPassword)
+                }
+                sign(publishing.publications["release"])
+                sign(configurations["archives"])
             }
         }
     }
