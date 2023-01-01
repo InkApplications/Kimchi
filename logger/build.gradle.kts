dafc6d6ccfac08a7b41adf0acb987210443c7dac
Renee Vandervelde <Renee@ReneeVandervelde.com>
2023-01-01 10:51:53 -0600

    Update publishing plugin

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     id("ink.multiplatform")
     id("com.android.library")
-    id("maven-publish")
-    id("signing")
+    id("com.inkapplications.publishing")
 }
 
 android {
@@ -38,58 +37,6 @@ kotlin {
                 implementation(libs.kotlin.test.core)
                 implementation(libs.kotlin.test.junit)
             }
-        }
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            val versionArg = when (project.properties["version"]?.toString()) {
-                null, "unspecified", "" -> "1.0-SNAPSHOT"
-                else -> project.properties["version"].toString()
-            }
-
-            create<MavenPublication>("release") {
-                from(components["release"])
-                version = versionArg
-            }
-        }
-        repositories {
-            maven {
-                name = "Build"
-                url = uri(layout.buildDirectory.dir("repo"))
-            }
-
-            val mavenUser: String? by project
-            val mavenPassword: String? by project
-            if (mavenUser != null && mavenPassword != null) {
-                maven {
-                    name = "MavenCentral"
-                    url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                    credentials {
-                        username = mavenUser
-                        password = mavenPassword
-                    }
-                }
-            }
-        }
-    }
-}
-publishing {
-    signing {
-        val signingKeyId: String? by project
-        val signingKey: String? by project
-        val signingPassword: String? by project
-
-        if (signingKey != null) {
-            if (signingKeyId != null) {
-                useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-            } else {
-                useInMemoryPgpKeys(signingKey, signingPassword)
-            }
-            sign(publishing.publications)
-            sign(configurations["archives"])
         }
     }
 }
